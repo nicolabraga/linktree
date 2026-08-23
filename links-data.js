02d797d5bd47f9d673a7c471efb6ee3d7eab9bda
Nicola Braga <nicola.emfocosurf@gmail.com>
2026-08-23 08:55:17 -0300

    Auto-update 23/08/2026 08:55

--- links-data.js
+++ links-data.js
@@ -1,9 +1,10 @@
 // Gerado automaticamente por update_links.py
 // Fonte: https://fotopix.com.br/
-// Atualizado em: 22/08/2026 19:06
+// Atualizado em: 23/08/2026 08:54
 window.LINKS_DATA = {
-  "updated_at": "22/08/2026 19:06",
+  "updated_at": "23/08/2026 08:54",
   "links": [
+    {"href": "https://fotopix.com.br/album/25e627", "text": "Beira Mar - Fortaleza - 23/08/2026"},
     {"href": "https://fotopix.com.br/album/25e61f", "text": "Beira Mar - Tarde/Noite - Fortaleza - 22/08/2026"},
     {"href": "https://fotopix.com.br/album/25e61d", "text": "Beira Mar - Fortaleza - 22/08/2026"},
     {"href": "https://fotopix.com.br/album/25e614", "text": "Beira Mar - Tarde/Noite - Fortaleza - 21/08/2026"},
